a5ef6a7cbdfdb20fa4986e09054291635cd228a0
neuron <nblue@towel.local>
2019-05-02 10:09:07 +1000

    Adjusted layout on disk Added regression demo

---
+++ build_environment.sh
@@ -0,0 +1,7 @@
+
+conda create -n auscert19 python=3 python keras jupyter pytest python-graphviz seaborn
+conda activate auscert19
+
+pip install xgboost
+pip install matplotlib
+pip install sklearn
---
+++ test_environment.py
@@ -0,0 +1,4 @@
+def test_xgboost_installed():
+    import xgboost
+
+    
