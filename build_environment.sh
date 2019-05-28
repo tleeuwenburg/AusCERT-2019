446d6d9c3d8ba4516ddb01acf6cdec8d4702b167
Tennessee <tleeuwenburg@gmail.com>
2019-05-28 17:39:38 +1000

    Added behavioural tutorials

--- build_environment.sh
+++ build_environment.sh
@@ -3,9 +3,11 @@ conda create -n auscert19 python=3 python keras jupyter pytest python-graphviz s
 conda activate auscert19
 
 pip install xgboost
+pip install matplotlib
 pip install sklearn
 pip install email_reply_parser
 pip install markovify
 pip install ip2geo
-
+pip install folium
+pip install geopy
 
