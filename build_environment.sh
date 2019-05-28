dead56f82b805f06cbfb8d5d1699a68ba3c51388
Tennessee <tleeuwenburg@gmail.com>
2019-05-28 11:03:12 +1000

    Simple tweaks

--- build_environment.sh
+++ build_environment.sh
@@ -1,10 +1,11 @@
 
-conda create -n auscert19 python=3 python keras jupyter pytest python-graphviz seaborn
+conda create -n auscert19 python=3 python keras jupyter pytest python-graphviz seaborn pillow matplotlib
 conda activate auscert19
 
 pip install xgboost
-pip install matplotlib
 pip install sklearn
 pip install email_reply_parser
 pip install markovify
 pip install ip2geo
+
+
